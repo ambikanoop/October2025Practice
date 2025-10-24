9daa016194cd94b2ecf6b5b182811f1d54e10a66
ambikanoop <ck.ambika@gmail.com>
2025-10-24 12:09:31 -0400

    third commit

--- src/test/java/TestingPackage/Test2.java
+++ src/test/java/TestingPackage/Test2.java
@@ -1,10 +1,17 @@
 package TestingPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
+		driver.manage().window().maximize();
+		String title=driver.getTitle();
+		System.out.println("page title is:"+title);
 	}
 
 }
